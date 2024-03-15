88c7048985f86da14602a0a96087d363efc62f02
AliceGonzalez <alicegcodes@gmail.com>
2024-03-15 16:28:06 -0400

    Started Lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,32 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
+
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
+    public Integer selection(Integer startIndex, String selection) {
+        Integer nextSong = (startIndex + 2) % playList.length;
+        Integer previousSong = (startIndex - 1 + playList.length) % playList.length;
+
+        int i;
+        for (i = startIndex; i < startIndex + playList.length * 2; i++) ;
+        if (playList[nextSong].equals(selection)){
+        return i;
+        }else {
+            if (playList[previousSong].equals(selection)) {
+                return i;
+            }
+        }
         return null;
     }
+
+
 }
