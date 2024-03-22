ffb0c378aaa012048736ba62847bae0590acd2e3
AliceGonzalez <alicegcodes@gmail.com>
2024-03-22 18:45:20 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,28 +5,21 @@ import java.util.*;
 
 public class Music {
 
-    private String[] playList;
+    private String playlist;
 
     public Music(String[] playList) {
 
-        this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection) {
-        Integer nextSong = (startIndex + 2) % playList.length;
-        Integer previousSong = (startIndex - 1 + playList.length) % playList.length;
-
-        int i;
-        for (i = startIndex; i < startIndex + playList.length * 2; i++) ;
-        if (playList[nextSong].equals(selection)){
-        return i;
-        }else {
-            if (playList[previousSong].equals(selection)) {
-                return i;
-            }
-        }
-        return null;
+        //Create song arrayList
+        ArrayList<String> songs = new ArrayList<String>(Arrays.asList(this.playlist));
+        //Calculate from selected song to start index
+        int up = Math.abs(songs.indexOf(selection) - startIndex);
+        //calculates the difference between the size of the songs list and the last occurrence index of the selection song added to the startIndex
+        int down = Math.abs(songs.size() + startIndex) - songs.lastIndexOf(selection);
+        //Returns the minimum of the two differences calculated in steps 2 and 3, which indicates the shortest distance (number of positions) to reach the selection song from either the starting index or the end of the list.
+        return Math.min(up, down);
+        
     }
-
-
 }
